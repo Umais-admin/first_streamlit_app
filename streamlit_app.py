7806f61fd61f6dc8c36b16b3fa23c50992d9a5cc
Umais-admin <136999631+Umais-admin@users.noreply.github.com>
2023-06-21 04:21:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,20 +21,21 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-# new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-      streamlit.error("please select a fruit to get information.") 
+      streamlit.error("please select a fruit to get information") 
    else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
+     streamlit.error()
+streamlit.write('The user entered ', fruit_choice)
+# import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
-# import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
@@ -58,7 +59,4 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-def insert_snowflake(new_fruit):
-    with my_cnx.cursor() as_my_cur:
-    my_cur.execute("insert into fruit_load_list values('"jackfruit","papaya","kiwi"."guava"')")
 
